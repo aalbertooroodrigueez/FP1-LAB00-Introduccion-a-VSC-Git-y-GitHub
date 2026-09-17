b6f6c0612dd73ea2e19b281d989f78681f91f44a
aalbertooroodrigueez <albrodmun@alum.us.es>
2026-09-17 12:25:33 +0200

    Implement time-based greeting in saludo.py
    Add greeting based on current hour and user input

--- src/saludo.py
+++ src/saludo.py
@@ -1,1 +1,10 @@
+from datetime import datetime
 
+hora_actual = datetime.now().hour
+nombre = input("¿Cuál es tu nombre? ")
+if hora_actual < 12:
+  print("Buenos días, " nombre)
+elif hora_actual 12 <= hora_actual < 20:
+  print(f"Buenas tardes, " nombre)
+elif hora_actual <= 23:
+  print(f"Buenas noches, " nombre)
