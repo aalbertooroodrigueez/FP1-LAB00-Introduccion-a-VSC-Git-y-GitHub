91f6a3fd321482e0fad5a9e6440d527e5a64a12c
Alberto Rodríguez Muñoz <albrodmun@alum.us.es>
2026-09-17 12:41:07 +0200

    Añadir saludo segun la hora

--- src/saludo.py
+++ src/saludo.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 hora_actual = datetime.now().hour
+<<<<<<< HEAD
 nombre = input("¿Cuál es tu nombre? ")
 if hora_actual < 12:
   print("Buenos días, " nombre)
@@ -8,3 +9,14 @@ elif hora_actual 12 <= hora_actual < 20:
   print(f"Buenas tardes, " nombre)
 elif hora_actual <= 23:
   print(f"Buenas noches, " nombre)
+=======
+
+nombre = input("¿Cuál es tu nombre? ")
+
+if hora_actual < 12:
+    print(f"Buenos días,  {nombre}")
+elif 12 <= hora_actual < 20:
+     print(f"Buenas tardes, {nombre}")
+else:
+    print(f"Buenas noches,  {nombre}")    
+>>>>>>> 40a3cb5 (Añadir saludo segun la hora)
